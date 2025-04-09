4405f42d768ff8335f9c294c551347329a18a6f7
Aneela Chaudhry <aneela.chaudhry001@gmail.com>
2025-04-09 13:15:12 -0400

    Move and fix spam validation code into correct project structure

---
+++ src/main/java/cypress/Report.java
@@ -0,0 +1,32 @@
+package cypress;
+
+public class Report {
+    private double latitude;
+    private double longitude;
+    private String issueType;
+    private String description;
+
+    public Report(double latitude, double longitude, String issueType, String description) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.issueType = issueType;
+        this.description = description;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public String getIssueType() {
+        return issueType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
+
